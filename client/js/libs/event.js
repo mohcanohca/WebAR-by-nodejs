fcd93b6a1d0d84e7403fb740fffef5ac9cd84932
ssydz <925584224@qq.com>
2018-10-31 15:04:42 +0800

    修改client端目录结构，使用require模块化方式，添加发布订阅模块的position处理逻辑

--- client/js/libs/event.js
+++ client/js/libs/event.js
@@ -1,55 +1,58 @@
 /**
  * Created by Lenovo on 2018/3/13.
  */
-var EventManager = (function(){
-    var list = {},
+
+define(function () {
+    let list = {},
         listen,
         trigger,
         remove;
     //订阅事件
-    listen = function(key,fn){
+    listen = function (key, fn) {
         //若该事件类型不存在，为其创建一个存放回调函数的数组
-        if(!list[key]) {
+        if (!list[key]) {
             list[key] = [];
         }
         //将订阅该事件key的订阅者的回调函数存入
         list[key].push(fn);
     };
     //发布事件
-    trigger = function(){
+    trigger = function () {
         //获取发布的事件名key从list中取出该key对应的订阅者的回调函数
-        var key = Array.prototype.shift.call(arguments),
+        let key = Array.prototype.shift.call(arguments),
             fns = list[key];
-        if(!fns || fns.length === 0) {
+        if (!fns || fns.length === 0) {
             return false;
         }
         //遍历所有的订阅者，依次执行对应的回调函数
-        for(var i = 0, fn; fn = fns[i++];) {
-            fn.apply(this,arguments);
+        for (var i = 0, fn; fn = fns[i++];) {
+            fn.apply(this, arguments);
         }
     };
     //取消订阅
-    remove = function(key,fn){
-        var fns = list[key];
-        if(!fns) {
+    remove = function (key, fn) {
+        let fns = list[key];
+        if (!fns) {
             return false;
         }
         //若fn为空，说明取消对该事件的所有订阅
-        if(!fn) {
+        if (!fn) {
             fns && (fns.length = 0);
-        }else {
+        } else {
             //遍历key对应的回调函数数组，将数组中的fn去除
-            for(var i = fns.length - 1; i >= 0; i--){
-                var _fn = fns[i];
-                if(_fn === fn) {
-                    fns.splice(i,1);
+            for (let i = fns.length - 1; i >= 0; i--) {
+                let _fn = fns[i];
+                if (_fn === fn) {
+                    fns.splice(i, 1);
                 }
             }
         }
     };
+
     return {
         listen: listen,
         trigger: trigger,
-        remove: remove
+        remove: remove,
+        show: show
     }
-})();
+});
