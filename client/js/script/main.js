8d061bb1eddeca28b4d31d050638f5b6e7828590
ssydz <925584224@qq.com>
2018-11-12 21:05:03 +0800

    添加控制按钮，图像控制和触摸事件鼠标键盘事件控制

--- client/js/script/main.js
+++ client/js/script/main.js
@@ -11,56 +11,128 @@ require(['io', 'eventManager', 'mediaDevices', 'modelController'], function (io,
 
     // let loadmodel = modelController.init(640, 480, document.getElementById('WebGL-output'));
 
-    //监听到后台返回的目标对象的位置信息的处理
-    eventManager.listen('position', handlePosition);
     //打开摄像头后的处理
-    eventManager.listen('cameraOpened', sendData);
-    //显示虚拟物体，会将图像的四个角点信息传递给回调函数
-    eventManager.listen('locateModel', function (param) {
-        console.log(param);
-        modelController.locateModel(param);
-        // eventManager.remove('locateModel');
+    eventManager.listen('cameraOpened', renderVideo);
+
+    eventManager.listen('changeControl', handleChangeControl);
+
+    let currentController = 'imageControl';
+
+    function handleChangeControl(type) {
+        eventManager.remove('changeControl');//移除对于changeControl监听，稍后重新添加监听
+
+
+        currentController = type;
+        modelController.reset();
+
+        switch (type) {
+            case 'imageControl':
+                handleImageControl()
+                break;
+            case 'handControl':
+                handleHandControl()
+                break;
+            case 'sensorControl':
+                handleSensorControl()
+                break;
+            case 'audioControl':
+                handleAudioControl()
+                break;
+            default:
+                handleImageControl()
+        }
+        eventManager.listen('changeControl', handleChangeControl);
+    }
+
+    let controllers = document.getElementById('controllers');
+
+    let video;//存放视频流的DOM元素
+
+    controllers.addEventListener('click', function (e) {
+        let controllerID = e.target.id;
+        if (controllerID) {
+            eventManager.trigger('changeControl', controllerID);
+        }
     });
 
 
-    //连接服务器端，传输数据
-    const socket = io.connect('https://10.108.164.203:8081');
-    socket.on('frame', function (data) {
-        eventManager.trigger('position', data);
-    });
+    //图像识别交互控制
+    function handleImageControl() {
+        console.log('图像识别交互控制');
+
+        //监听到后台返回的目标对象的位置信息的处理
+        eventManager.listen('position', function (data) {
+            let corners = data.corners;
+            if (!corners) return;
+
+            //绘制目标图像的四个角点
+            // drawCorners(corners);
+            eventManager.trigger('locateModel', corners)
+        });
+
+        //显示虚拟物体，会将图像的四个角点信息传递给回调函数
+        eventManager.listen('locateModel', function (corners) {
+            modelController.locateModel(corners);
+            // eventManager.remove('locateModel');
+        });
+
+        let default_video_period = 100;
+        let video_period = 100;
+
+        //连接服务器端，传输数据
+        const socket = io.connect('https://10.108.164.203:8081');
+        socket.on('frame', function (data) {
+            eventManager.trigger('position', data);
+        });
+
+        //定时向后端传输图像数据
+        let timer = setInterval(function () {
+            if (currentController !== 'imageControl') {
+                clearInterval(timer);
+                timer = null;
+            } else {
+                sendVideoData(socket, video, video.videoWidth, video.videoHeight);
+            }
+        }, video_period || default_video_period);
+    }
+
+    //触摸屏、键盘、鼠标控制
+    function handleHandControl() {
+        console.log('触摸屏、键盘、鼠标控制')
+        modelController.handControl('haha');
+    }
+
+    //传感器控制
+    function handleSensorControl() {
+        console.log('传感器控制')
+    }
+
+    //语音控制
+    function handleAudioControl() {
+        console.log('语音控制')
+    }
+
 
     let canvas = document.getElementById('canvas');//用于绘制摄像头捕捉内容
-    let canvasFace = document.getElementById('canvas-face');//用于绘制目标图像出现的位置
-    let context_face = canvasFace.getContext('2d');
+    // let canvasFrame = document.getElementById('canvas-frame');//用于绘制目标图像出现的位置
+    // let canvas_frame = canvasFrame.getContext('2d');
+
 
     let defaultVideoWidth = 640;//设置默认值
     let defaultVideoHeight = 480;
 
 
-    //响应后台返回的位置信息
-    function handlePosition(data) {
-        // calTransformByIMU(imu, period);
-        // let rotation = null, transition = null;
-        let corners = data.corners;
-        if (!corners) return;
-
-        //绘制目标图像的四个角点
-        // drawCorners(corners);
-
-        eventManager.trigger('locateModel', corners)
-    }
-
     function drawCorners(corners) {
-        context_face.clearRect(0, 0, 640, 480);
-        context_face.fillStyle = "red";
+        canvas_frame.clearRect(0, 0, 640, 480);
+        canvas_frame.fillStyle = "red";
         let center;
         let sumx = 0;
         let sumy = 0;
         let count = 0;
         for (let corner of corners) {
-            context_face.beginPath();
-            context_face.arc(corner.x, corner.y, 3, 0, 2 * Math.PI);
-            context_face.fill();
+            canvas_frame.beginPath();
+            canvas_frame.arc(corner.x, corner.y, 3, 0, 2 * Math.PI);
+            canvas_frame.fill();
             // if (corner.x < 0 || corner.x > 640 || corner.y < 0 || corner.y > 480) continue;
             sumx += corner.x;
             sumy += corner.y;
@@ -122,8 +194,8 @@ require(['io', 'eventManager', 'mediaDevices', 'modelController'], function (io,
         eventManager.trigger('cameraOpened', cameraDeviceIds);
     });
 
-    //前后端通信
-    function sendData(cameraDeviceIds, videoConstraints, video_period) {
+    //获取视频流
+    function renderVideo(cameraDeviceIds, videoConstraints) {
         let defaultVideoConstraints = {
             width: defaultVideoWidth,
             height: defaultVideoHeight,
@@ -131,11 +203,11 @@ require(['io', 'eventManager', 'mediaDevices', 'modelController'], function (io,
         };
 
         let constraints = {video: videoConstraints || defaultVideoConstraints};
-        let default_video_period = 100;
+        // let default_video_period = 100;
 
         mediaDevices.getUserMedia(constraints)
             .then(function (stream) {
-                let video = document.getElementById('video');
+                video = document.getElementById('video');
                 // 旧的浏览器可能没有srcObject
                 if ("srcObject" in video) {
                     video.srcObject = stream;
@@ -149,12 +221,12 @@ require(['io', 'eventManager', 'mediaDevices', 'modelController'], function (io,
                     // height = video.videoHeight;
 
                     //初始化webgl相关
-                    modelController.onload(video,eventManager);
+                    modelController.onload(video, eventManager);
 
                     //定时向后端传输图像数据和imu数据
-                    setInterval(function () {
+                    /*setInterval(function () {
                         sendVideoData(video, video.videoWidth, video.videoHeight);
-                    }, video_period || default_video_period);
+                    }, video_period || default_video_period);*/
                 };
 
             }).catch(function (err) {
@@ -163,15 +235,14 @@ require(['io', 'eventManager', 'mediaDevices', 'modelController'], function (io,
     }
 
     //发送视频帧
-    function sendVideoData(video, width, height) {
-
+    function sendVideoData(socket, video, width, height) {
         // if (width && height) {
         canvas.width = width || defaultVideoWidth;
         canvas.height = height || defaultVideoHeight;
 
         let context = canvas.getContext('2d');
-        canvasFace.width = width;
-        canvasFace.height = height;
+        // canvasFrame.width = width;
+        // canvasFrame.height = height;
 
         //绘制当前视频帧
         context.drawImage(video, 0, 0, width, height, 0, 0, width, height);
